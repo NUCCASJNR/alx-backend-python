888ccfab5a25d84a7d7041ee028a1d9929ed5200
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-03 18:56:46 +0100

    reduck

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -7,4 +7,5 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Ducks an iterable"""
     return [(i, len(i)) for i in lst]
