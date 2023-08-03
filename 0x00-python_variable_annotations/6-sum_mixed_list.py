306e5e02fd20977827670ddab0af5d303193e374
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-03 14:00:25 +0100

    Added union to list type since the checker uses  python3.7 to run the code

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,10 +5,10 @@ This module contains a type-annotated function that takes in multiple data
 types and returns their sum as a float
 """
 
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     a type-annotated function sum_mixed_list which takes a list mxd_lst of
     integers and floats and returns their sum as a float.
