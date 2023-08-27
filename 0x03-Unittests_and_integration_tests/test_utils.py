ccbc2fd7fc1b2be247ca8e7cdeff6b83aee6e704
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-27 05:34:39 +0100

    Mock HTTP call using patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,9 +5,11 @@ This module tests the utils.access_nested_map function
 """
 
 import unittest
+from typing import Dict
+from unittest.mock import Mock, patch
 
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -32,6 +34,27 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(output) as err:
             result = access_nested_map(nested_map, path)
         # self.assertEqual(str(err.exception), output)
+
+
+class TestCaseJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, url, payload, mock_get):
+        """
+        TestCase for get_json method
+        """
+        # Define a mock obj with json response
+        mock_obj = Mock()
+        mock_obj.json.return_value = payload
+        # Confogure the mock_get function to return the mock_obj
+        mock_get.return_value = mock_obj
+        # Call the json function on the parametrized urls
+        result = get_json(url)
+        mock_get.assert_called_once_with(url)
+        self.assertEqual(result, payload)
 
 
 if __name__ == "__main__":
