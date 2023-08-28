a7678dfaa4e62f871c88962c0aa28b0976c62d57
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-28 11:10:37 +0100

    GithubClient

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 This module tests client.py
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 
 from client import GithubOrgClient
@@ -20,7 +20,22 @@ class TestGithubOrgClient(unittest.TestCase):
         with patch('client.get_json', return_value=output) as mock_get_json:
             client = GithubOrgClient(org_name)
             result = client.org
+            # print(result)
             mock_get_json.assert_called_once_with(expected_url)
+            self.assertEqual(result, output)
+
+    @parameterized.expand([
+        ("google", ("https://api.github.com/orgs/adobe/repos")),
+        ("abc", ("https://api.github.com/orgs/abc/repos")),
+        ('Netflix', ("https://api.github.com/orgs/Netflix/repos"))
+    ])
+    def test_public_repos_url(self, org_name, output):
+        """Test the GithubOrgClient.public-repos_url method"""
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock)as mock_public_repos_url:
+            mock_public_repos_url.return_value = output
+            client = GithubOrgClient(org_name)
+            result = client._public_repos_url
             self.assertEqual(result, output)
 
 
